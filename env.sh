2f46a8aae4aba944e8bf1325e039314ea3e2e1d3
kaspermunch <kaspermunch@birc.au.dk>
2020-12-19 11:04:26 +0100

    Initial commit

---
+++ env.sh
@@ -0,0 +1,3 @@
+
+conda create --name baboons gwf jupyter jupyterlab nbconvert ipyparallel numpy scipy pandas modin h5py statsmodels matplotlib seaborn geoplot cartopy ipympl biopython pyfaidx mygene scikit-allel pylint vcftools tabix samtools plink ipytree bcftools nodejs openpyxl
+
